7c97b37d655032bc599491a6d5110aef7f797768
Dovudhon Husanov <95928909+husanovdovudkhon@users.noreply.github.com>
2022-01-12 16:50:44 +0300

    js -page

---
+++ script.js
@@ -0,0 +1,26 @@
+var comingdate = new Date("2556 326 1526 856")
+
+var d  = document.getElementById ('d');
+var h  = document.getElementById ('h');
+var m  = document.getElementById ('m');
+var s  = document.getElementById ('s');
+
+var x = setInterval(function(){
+    var now = new Date();
+    var des = comingdate.getTime() - now.getTime();
+    var days = Math.floor(des/(1000 * 60 * 60 * 24));
+    var hours = Math.floor(des%(1000 * 60 * 60 * 24) / (1000 * 60 * 60 ));
+    var mins = Math.floor(des%(1000 * 60 * 60) / (1000 * 60));
+    var secs = Math.floor(des%(1000 * 60) / 1000);
+
+  d.innerHTML = getTrueNumber(days);
+  h.innerHTML = getTrueNumber(hours);
+  m.innerHTML = getTrueNumber(mins);
+  s.innerHTML = getTrueNumber(secs);
+
+},1000);
+
+function getTrueNumber (x) {
+    if(x<10) return '0'+x;
+    else return x;
+}
